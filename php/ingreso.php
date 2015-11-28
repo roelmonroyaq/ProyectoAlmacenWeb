6a1d0b4309264d9c0fff775b3f5aa5d898db4469
Carlos Hernandez <caedhernandezve@ittepic.edu.mx>
2015-11-28 15:46:30 -0700

    subida

---
+++ php/ingreso.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<!DOCTYPE HTML>
+<html>
+<header>
+  <title>Ingreso Al sistema</title>
+  <link rel="stylesheet" type="text/css" href="css/ingreso.css">
+  <meta charset="utf-8">
+</header>
+<body>
+  <h1>Bienvenido al sistema</h1>
+  <div class="Ingreso">
+    <p>
+      Usuario
+    </p>
+     <input type="text" id="usuario" placeholder="usuario">
+    <p>
+      Contraseña
+    </p>
+    <input type="password" id="pass" placeholder="Contraseña">
+
+    <button type="button" id="acceder" >Acceder</button>
+  </div>
+  <script type="text/javascript" href="js/ingreso.js">
+  </script>
+</body>
+</html>
+
+<!--
+<div class='login'>
+  <h2>Sign in</h2>
+  <input name='username' placeholder='Username' type='text'/>
+  <input id='pw' name='password' placeholder='Password' type='password'/>
+  <div class='remember'>
+    <input checked='' id='remember' name='remember' type='checkbox'/>
+    <label for='remember'></label>Remember me
+  </div>
+  <input type='submit' value='Sign in'/>
+  <a class='forgot' href='#'>Forgot your password?</a>
+</div>
+
+<script class="cssdeck" src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+-->
